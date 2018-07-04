eebe808a029243d44a57664046e4ab954b50e58c
John Clark <inindev@gmail.com>
2018-07-04 07:19:25 -0400

    add cursor and display control

--- hd44780/HD44780.py
+++ hd44780/HD44780.py
@@ -12,9 +12,35 @@
 #     sudo i2cdetect -y 0
 #     sudo i2cdetect -y 1
 #
+#
+#    MIT License
+#
+#    Copyright (c) 2018 John Clark
+#
+#    Permission is hereby granted, free of charge, to any person obtaining a copy
+#    of this software and associated documentation files (the "Software"), to deal
+#    in the Software without restriction, including without limitation the rights
+#    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#    copies of the Software, and to permit persons to whom the Software is
+#    furnished to do so, subject to the following conditions:
+#
+#    The above copyright notice and this permission notice shall be included in all
+#    copies or substantial portions of the Software.
+#
+#    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+#    SOFTWARE.
+#
 
 from time import sleep
 import smbus
+
+Left = False
+Right = True
 
 
 class HD44780(object):
@@ -70,6 +96,8 @@ class HD44780(object):
         self.addr = addr
         self.i2c = smbus.SMBus(bus)
         self.backlight = True
+        self._display = 0
+        self._entry_mode = 0
 
         # init to 4 bit mode (p.46)
         self.write_reg(self.REG_D5 | self.REG_D4)
@@ -84,14 +112,16 @@ class HD44780(object):
                                          | self.FUNCTION_1LINE if rows==1 else self.FUNCTION_2LINE)
 
         # display / cursor / blink
-        self.write_cmd(self.DISPLAY_SET | self.DISPLAY_ON | self.DISPLAY_CURSOR_OFF | self.DISPLAY_BLINK_OFF)
+        self._display = self.DISPLAY_ON | self.DISPLAY_CURSOR_OFF | self.DISPLAY_BLINK_OFF
+        self.write_cmd(self.DISPLAY_SET | self._display)
 
         # clear display
         self.write_cmd(self.CLEAR_DISPLAY)
         sleep(0.00167)  # wait >1.52ms (p.24)
 
         # text direction & scroll
-        self.write_cmd(self.ENTRY_MODE_SET | self.ENTRY_MODE_SHLEFT | self.ENTRY_MODE_DEC)
+        self._entry_mode = self.ENTRY_MODE_SHLEFT | self.ENTRY_MODE_DEC
+        self.write_cmd(self.ENTRY_MODE_SET | self._entry_mode)
 
     def write_reg(self, reg):
         self.i2c.write_byte(self.addr, reg |  self.REG_EN)  # EN high
@@ -125,11 +155,83 @@ class HD44780(object):
         self.write_cmd(self.RETURN_HOME)
         sleep(0.00167)  # wait >1.52ms (p.24)
 
+    # backlight - note that backlight is a first-class property
+
+    @property
+    def display(self):
+        return self.DISPLAY_ON == (self._display & self.DISPLAY_ON)
+    @display.setter
+    def display(self, val):
+        if val:
+            self._display |= self.DISPLAY_ON
+        else:
+            self._display &= ~self.DISPLAY_ON
+        self.write_cmd(self.DISPLAY_SET | self._display)
+
+    @property
+    def cursor(self):
+        return self.DISPLAY_CURSOR_ON == (self._display & self.DISPLAY_CURSOR_ON)
+    @cursor.setter
+    def cursor(self, val):
+        if val:
+            self._display |= self.DISPLAY_CURSOR_ON
+        else:
+            self._display &= ~self.DISPLAY_CURSOR_ON
+        self.write_cmd(self.DISPLAY_SET | self._display)
+
+    @property
+    def cursor_blink(self):
+        return self.DISPLAY_BLINK_ON == (self._display & self.DISPLAY_BLINK_ON)
+    @cursor_blink.setter
+    def cursor_blink(self, val):
+        if val:
+            self._display |= self.DISPLAY_BLINK_ON
+        else:
+            self._display &= ~self.DISPLAY_BLINK_ON
+        self.write_cmd(self.DISPLAY_SET | self._display)
+
+    @property
+    def scroll_lock(self):
+        return self.ENTRY_MODE_INC == (self._entry_mode & self.ENTRY_MODE_INC)
+    @scroll_lock.setter
+    def scroll_lock(self, val):
+        if val:
+            self._entry_mode |= self.ENTRY_MODE_INC
+        else:
+            self._entry_mode &= ~self.ENTRY_MODE_INC
+        self.write_cmd(self.ENTRY_MODE_SET | self._entry_mode)
+
+    @property
+    def left_to_right(self):
+        return self.ENTRY_MODE_SHLEFT == (self._entry_mode & self.ENTRY_MODE_SHLEFT)
+    @left_to_right.setter
+    def left_to_right(self, val):
+        if val:
+            self._entry_mode |= self.ENTRY_MODE_SHLEFT
+        else:
+            self._entry_mode &= ~self.ENTRY_MODE_SHLEFT
+        self.write_cmd(self.ENTRY_MODE_SET | self._entry_mode)
+
+    @property
+    def right_to_left(self):
+        return not self.left_to_right
+    @right_to_left.setter
+    def right_to_left(self, val):
+        self.left_to_right = not val
+
+    def scroll_cursor(self, cols=1, dir=Right):
+        for col in range(cols):
+            self.write_cmd(self.CURSOR_SHIFT | self.DC_CURSOR_MOVE | self.DC_MOVE_RIGHT if dir else self.DC_MOVE_LEFT)
+
+    def scroll_display(self, cols=1, dir=Right):
+        for col in range(cols):
+            self.write_cmd(self.CURSOR_SHIFT | self.DC_DISPLAY_MOVE | self.DC_MOVE_RIGHT if dir else self.DC_MOVE_LEFT)
+
 
 def main():
 #    lcd = HD44780(rows=2, cols=16, addr=0x27)
     lcd = HD44780(rows=4, cols=20, addr=0x3f)
-    lcd.set_cursor(3, 0)
+    lcd.set_cursor(3, 2)
     lcd.write_text('HD44780 is cool!')
 
     from datetime import datetime
@@ -149,5 +251,6 @@ if __name__ == '__main__':
     try:
         main()
     except KeyboardInterrupt:
+        print(' break')
         pass
 
